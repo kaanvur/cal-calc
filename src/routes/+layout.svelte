9d010d161e187e0ab3ef60f6941357580fecc3ea
Kaan Vurgun <kaan.vurgun@enoca.com>
2024-03-11 13:52:25 +0300

    add install as app bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
 	import '../app.pcss';
 	import { DarkMode } from 'flowbite-svelte';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Banner } from 'flowbite-svelte';
+	import {DeviceDesktopDown} from 'svelte-tabler';
 </script>
 
 <svelte:head>
@@ -25,3 +26,15 @@
 </Navbar>
 
 <slot />
+<Banner id="bottom-banner" position="absolute" bannerType="bottom">
+  <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+    <span class="inline-flex p-1 me-3 bg-gray-200 rounded-full dark:bg-gray-600">
+	<DeviceDesktopDown class="w-4 h-4 text-gray-500 dark:text-gray-400" />
+
+      <span class="sr-only">Uygulama olarak yükle</span>
+    </span>
+    <span>
+      Uygulama olarak yükleyerek kolay erişim sağlayabilirsiniz. 
+    </span>
+  </p>
+</Banner>
